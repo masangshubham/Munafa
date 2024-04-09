f251359406bb0df5f8317b4ac74c356dba0992d0
masangshubham <shubhamm_t20018@students.isquareit.edu.in>
2024-04-09 13:34:17 +0530

    final commit

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import LoginScreen from "./components/LoginScreen"; // Adjust the path as necessary
-import HomeScreen from "./components/HomeScreen"; // Adjust the path as necessary
+import LoginScreen from "./components/LoginScreen";
+import HomeScreen from "./components/HomeScreen"; 
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import RegisterScreen from "./components/RegisterScreen"; // Adjust the path as necessary
+import RegisterScreen from "./components/RegisterScreen";
 import { LogBox } from "react-native";
 const Stack = createStackNavigator();
 
 export default function App() {
   LogBox.ignoreAllLogs();
-  const [isSignedIn, setIsSignedIn] = useState(true);
+  const [isSignedIn, setIsSignedIn] = useState(false);
   const [userNameApp, setUserNameApp] = useState("");
   return (
     <SafeAreaProvider>
